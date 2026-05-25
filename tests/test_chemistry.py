fe56655b84a07f0caa319bc21ed53b4ce39b1798
Juan Alday <juanaldayparejo@gmail.com>
2026-05-25 17:35:11 +0200

    update tests for mass conservation check

--- tests/test_chemistry.py
+++ tests/test_chemistry.py
@@ -115,12 +115,28 @@ def test_mass_reactions():
     h = np.zeros(3) ; p = np.ones(3) ; t = np.ones(3)
     n = np.ones((3,4))
 
-    #Calculating all available reactions
-    rtype, ns, sf, sID, sISO, npr, pf, pID, pISO, rrates = isochem.chemistry.reaction_rate_coefficients(reaction_ids, gasID, isoID, h, p, t, n)
+    #Calculating all available reactions with 15N chemistry
+    rtype, ns, sf, sID, sISO, npr, pf, pID, pISO, rrates = isochem.chemistry.reaction_rate_coefficients(reaction_ids, gasID, isoID, h, p, t, n, include_15n=True)
     
-    for ir in range(len(reaction_ids)):
+    for ir in range(len(rtype)):
 
-        
+        #Calculating the mass of the parents
+        m_parents = 0.
+        for ip in range(ns[ir]):
+            m_parents += isochem.dict.gas_dict.get_molwt(sID[ip,ir],sISO[ip,ir]) * sf[ip,ir]
+        m_products = 0.
+        for ip in range(npr[ir]):
+            m_products += isochem.dict.gas_dict.get_molwt(pID[ip,ir],pISO[ip,ir]) * pf[ip,ir]
+
+        print(ir,m_parents,m_products,pf[:,ir])
+
+        assert np.allclose(m_parents,m_products,rtol=1.0e-6)
+
+    #Calculating all available reactions with 13C chemistry 
+    rtype, ns, sf, sID, sISO, npr, pf, pID, pISO, rrates = isochem.chemistry.reaction_rate_coefficients(reaction_ids, gasID, isoID, h, p, t, n, include_13c=True)
+    
+    for ir in range(len(rtype)):
+
         #Calculating the mass of the parents
         m_parents = 0.
         for ip in range(ns[ir]):
